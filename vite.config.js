8cbb5652f17e497ee37c9c61139222c4ba0c6889
Mohit Sharma <starmohit14@gmail.com>
2025-06-26 13:20:05 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Todo-App/',
+  base: '/Todo-App/', // ✅ This must match repo name
   plugins: [react()],
 })
